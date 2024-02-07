eba39bf6aa3c9d69510156b1069b1c165949fae3
irensula <irinawebdev@gmail.com>
2024-02-07 22:46:30 +0200

    my first sql database

--- irysul23_demot.sql
+++ irysul23_demot.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: 08.01.2024 klo 11:52
--- Palvelimen versio: 8.0.35
--- PHP Version: 8.1.26
+-- Host: 127.0.0.1
+-- Generation Time: Feb 07, 2024 at 09:44 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,179 +18,96 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `irysul23_demot`
+-- Database: `myfirstdatabase`
 --
 
 -- --------------------------------------------------------
 
 --
--- Rakenne taululle `Artists`
+-- Table structure for table `comments`
 --
 
-CREATE TABLE `Artists` (
-  `ArtistID` int NOT NULL,
-  `Name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `ContactEmail` varchar(254) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `ContactPhone` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `StyleID` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `comment_text` text NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT curtime(),
+  `users_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Vedos taulusta `Artists`
+-- Dumping data for table `comments`
 --
 
-INSERT INTO `Artists` (`ArtistID`, `Name`, `ContactEmail`, `ContactPhone`, `StyleID`) VALUES
-(1, 'Zemfira', 'zemfira@gmail.com', '0501234567', 1),
-(2, '50cent', '50cent@gmail.com', '0501234568', 3),
-(3, 'Nora Jhones', 'norajhones@gmail.com', '0501234569', 2),
-(4, 'Alma', 'alma@gmail.com', '0501234567', 6),
-(5, 'Lauri Tähkä', 'lauritahka@gmail.com', '0501234570', 4);
+INSERT INTO `comments` (`id`, `username`, `comment_text`, `created_at`, `users_id`) VALUES
+(1, 'Krossing', 'This is a comment on a website.', '2024-02-07 13:09:39', 3);
 
 -- --------------------------------------------------------
 
 --
--- Rakenne taululle `Gigs`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `Gigs` (
-  `GigID` int NOT NULL,
-  `Date` date NOT NULL,
-  `Showtime` time NOT NULL,
-  `AgeLimit` int NOT NULL,
-  `VenuelID` int NOT NULL,
-  `ArtistID` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `pwd` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT curtime()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Vedos taulusta `Gigs`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `Gigs` (`GigID`, `Date`, `Showtime`, `AgeLimit`, `VenuelID`, `ArtistID`) VALUES
-(1, '2024-01-19', '19:00:00', 18, 1, 4),
-(2, '2024-01-20', '19:00:00', 18, 2, 1),
-(3, '2024-01-25', '19:00:00', 18, 2, 5),
-(4, '2024-01-26', '19:00:00', 18, 1, 3);
-
--- --------------------------------------------------------
-
---
--- Rakenne taululle `Styles`
---
-
-CREATE TABLE `Styles` (
-  `StyleID` int NOT NULL,
-  `StayleName` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Vedos taulusta `Styles`
---
-
-INSERT INTO `Styles` (`StyleID`, `StayleName`) VALUES
-(1, 'Rock'),
-(2, 'Jazz'),
-(3, 'Rap'),
-(4, 'Pop'),
-(5, 'Classic'),
-(6, 'Iskelmä');
-
--- --------------------------------------------------------
-
---
--- Rakenne taululle `Venues`
---
-
-CREATE TABLE `Venues` (
-  `VenueID` int NOT NULL,
-  `Name` varchar(100) NOT NULL,
-  `Address` varchar(50) NOT NULL,
-  `PostalCode` int NOT NULL,
-  `PostalAddress` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Vedos taulusta `Venues`
---
-
-INSERT INTO `Venues` (`VenueID`, `Name`, `Address`, `PostalCode`, `PostalAddress`) VALUES
-(1, 'Nokia Areena', 'Keskustankatu 11', 12300, 'Tampere'),
-(2, 'TampereTalo', 'Keskustankatu 100', 12301, 'Tampere');
+INSERT INTO `users` (`id`, `username`, `pwd`, `email`, `created_at`) VALUES
+(2, 'Basse', 'basse123', 'basse@gmail.com', '2024-02-07 12:51:12'),
+(3, 'Krossing', 'dani123', 'johndoe@gmail.com', '2024-02-07 13:02:00'),
+(4, 'Irina', '1234', 'irensula19@gmail.com', '2024-02-07 15:47:50'),
+(5, 'Karina', '12345', 'karina@gmail.com', '2024-02-07 15:47:55');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `Artists`
+-- Indexes for table `comments`
 --
-ALTER TABLE `Artists`
-  ADD PRIMARY KEY (`ArtistID`),
-  ADD KEY `StyleID` (`StyleID`);
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `users_id` (`users_id`);
 
 --
--- Indexes for table `Gigs`
+-- Indexes for table `users`
 --
-ALTER TABLE `Gigs`
-  ADD PRIMARY KEY (`GigID`),
-  ADD KEY `ArtistID` (`ArtistID`),
-  ADD KEY `VenuelID` (`VenuelID`);
-
---
--- Indexes for table `Styles`
---
-ALTER TABLE `Styles`
-  ADD PRIMARY KEY (`StyleID`);
-
---
--- Indexes for table `Venues`
---
-ALTER TABLE `Venues`
-  ADD PRIMARY KEY (`VenueID`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `Artists`
+-- AUTO_INCREMENT for table `comments`
 --
-ALTER TABLE `Artists`
-  MODIFY `ArtistID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `Gigs`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `Gigs`
-  MODIFY `GigID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `Styles`
---
-ALTER TABLE `Styles`
-  MODIFY `StyleID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `Venues`
---
-ALTER TABLE `Venues`
-  MODIFY `VenueID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Rajoitteet vedostauluille
+-- Constraints for dumped tables
 --
 
 --
--- Rajoitteet taululle `Artists`
+-- Constraints for table `comments`
 --
-ALTER TABLE `Artists`
-  ADD CONSTRAINT `Artists_ibfk_1` FOREIGN KEY (`StyleID`) REFERENCES `Styles` (`StyleID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Rajoitteet taululle `Gigs`
---
-ALTER TABLE `Gigs`
-  ADD CONSTRAINT `Gigs_ibfk_1` FOREIGN KEY (`ArtistID`) REFERENCES `Artists` (`ArtistID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `Gigs_ibfk_2` FOREIGN KEY (`VenuelID`) REFERENCES `Venues` (`VenueID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
